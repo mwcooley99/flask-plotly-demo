ce8fa2e485d5f1c1486d7243e7e4d12ccccf935f
Matt <mwcooley99@gmail.com>
2019-05-30 17:55:24 -0700

    minor cleaning

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, jsonify
+from flask import Flask, render_template, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 
